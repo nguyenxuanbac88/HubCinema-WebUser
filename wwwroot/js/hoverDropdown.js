25c31c384dedcc305ce5a33f1ee1c2740a7cc94b
NguyenDamKha <nguyendamkha@gmail.com>
2025-06-22 23:55:57 +0700

    fix dropdown list hover effect

---
+++ wwwroot/js/hoverDropdown.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const navbarDropdowns = document.querySelectorAll('.navbar-dropdown');
+
+    navbarDropdowns.forEach(container => {
+        const dropdown = container.querySelector('.dropdown');
+        const menu = dropdown.querySelector('.dropdown-menu');
+
+        let timeout;
+
+        container.addEventListener('mouseenter', () => {
+            clearTimeout(timeout);
+            menu.style.display = 'block';
+            menu.style.opacity = '1';
+            menu.style.visibility = 'visible';
+        });
+
+        container.addEventListener('mouseleave', () => {
+            timeout = setTimeout(() => {
+                menu.style.display = 'none';
+                menu.style.opacity = '0';
+                menu.style.visibility = 'hidden';
+            }, 200); // nhỏ delay để tránh mất menu khi rê nhanh
+        });
+    });
+});
