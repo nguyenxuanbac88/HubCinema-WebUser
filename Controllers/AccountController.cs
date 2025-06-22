f4f2fe17347a905617ee48f3eeaa5cd1d28a3dea
NguyenDamKha <nguyendamkha@gmail.com>
2025-06-23 01:36:00 +0700

    add account/profile page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTicketWebsite.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MovieTicketWebsite.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AccountController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            var token = HttpContext.Session.GetString("AccessToken");
+
+            if (string.IsNullOrEmpty(token))
+            {
+                TempData["LoginMessage"] = "Chưa đăng nhập.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            try
+            {
+                var response = await client.GetAsync("http://api.dvxuanbac.com:2030/Api/User/GetInfo");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    var userInfo = JsonConvert.DeserializeObject<UserInfo>(jsonString);
+                    return View(userInfo);
+                }
+                else
+                {
+                    TempData["LoginMessage"] = "Lỗi khi lấy thông tin người dùng.";
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["LoginMessage"] = "Lỗi kết nối: " + ex.Message;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace MovieTicketWebsite.Models
+{
+    public class UserInfo
+    {
+        public int IdUser { get; set; }
+        public string MaBarcode { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Dob { get; set; }
+        public string Gender { get; set; }
+        public int Diem { get; set; }
+        public int TongChiTieu { get; set; }
+        public string ThuHang { get; set; }
+        public string KhuVuc { get; set; }
+    }
+}
