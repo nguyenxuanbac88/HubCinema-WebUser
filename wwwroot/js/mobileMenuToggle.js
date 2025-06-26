fc2aab1fcce62b69dcaf5c5bfb2915b8d4f38da7
NguyenDamKha <nguyendamkha@gmail.com>
2025-06-26 19:41:26 +0700

    fix(mobile-sidebar): improve theater dropdown display

---
+++ wwwroot/js/mobileMenuToggle.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenu = document.getElementById('mobileMenu');
+    const menuToggle = document.getElementById('menuToggle');
+    const menuClose = document.getElementById('menuClose');
+
+    if (menuToggle && mobileMenu && menuClose) {
+        menuToggle.addEventListener('click', () => {
+            mobileMenu.style.transform = 'translateX(0)';
+        });
+
+        menuClose.addEventListener('click', () => {
+            mobileMenu.style.transform = 'translateX(100%)';
+        });
+    }
+});
