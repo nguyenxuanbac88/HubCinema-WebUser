0b3c2a97a4be53f44a5995ffa74918846b298776
Trần Duy Khoa <khoa992005@gmail.com>
2025-06-27 17:51:28 +0700

    update GetCinema

---
+++ wwwroot/js/GetCinema.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll(".tab-date");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", function () {
+            // Xoá class active khỏi tất cả
+            tabs.forEach(t => t.classList.remove("active"));
+
+            // Thêm class active vào tab đang click
+            this.classList.add("active");
+        });
+    });
+});
