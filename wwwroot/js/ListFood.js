39c5ced4cf457c6ce4f4d4e34c27324562be8f50
Trần Duy Khoa <khoa992005@gmail.com>
2025-07-14 15:02:29 +0700

    add see more and shorten

---
+++ wwwroot/js/ListFood.js
@@ -0,0 +1,13 @@
+﻿const toggleBtn = document.getElementById("toggleFoodBtn");
+const toggleText = toggleBtn.querySelector(".toggle-text");
+
+let expanded = false;
+
+toggleBtn.addEventListener("click", function () {
+    document.querySelectorAll(".food-hidden").forEach(el => {
+        el.style.display = expanded ? "none" : "block";
+    });
+
+    toggleText.textContent = expanded ? "Xem thêm" : "Rút gọn";
+    expanded = !expanded;
+});
