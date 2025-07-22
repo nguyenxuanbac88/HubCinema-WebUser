9c760362804322fc9df98db156e3850cad15f1ec
nguyenxuanbac88 <xuanbacatym@gmail.com>
2025-07-23 01:53:43 +0700

    -Feat: ticket detail

---
+++ Models/ProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieTicketWebsite.Models
+{
+    public class ProfileViewModel
+    {
+        public UserInfo User { get; set; }
+        public List<Invoice> Invoices { get; set; }
+    }
+
+}
